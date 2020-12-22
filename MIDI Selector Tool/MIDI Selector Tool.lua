b7c2d75d4969ff481e49a11ef8d8426aabca0e3f
lemerchand <phoenix.scooter@gmail.com>
2020-12-22 05:49:39 -0800

    k

--- MIDI Selector Tool/MIDI Selector Tool.lua
+++ MIDI Selector Tool/MIDI Selector Tool.lua
@@ -1,4 +1,4 @@
--- @version 1.0.8c
+-- @version 1.0.88c
 -- @author Lemerchand
 -- @provides
 --    [main=midi_editor] .
@@ -6,7 +6,7 @@
 --    [nomain] libs/*.lua
 --    [nomain] *.config
 --
-local v = " v1.0.8c"
+local v = " v1.0.88c"
 local name = "MST5K"
 local path = ""
 
