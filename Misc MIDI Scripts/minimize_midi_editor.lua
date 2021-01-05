246694e9e5002694bbc90e76b7f7c2130f5f6b7b
lemerchand <phoenix.scooter@gmail.com>
2021-01-05 12:46:00 -0800

    Signed-off-by: lemerchand <phoenix.scooter@gmail.com>

--- Misc MIDI Scripts/minimize_midi_editor.lua
+++ Misc MIDI Scripts/minimize_midi_editor.lua
@@ -3,13 +3,22 @@ reaperDoFile('ui.lua')
 reaperDoFile('../libss/cf.lua')
 reaper.ClearConsole()
 
-
-local btn_add = Button:Create(nil, 10, " ADD", nil, nil,nil,nil, 40, 28)
-local btn_clear = Button:Create(nil, btn_add.y+btn_add.h+3, " CLR", nil, nil, nil, nil, 40, 28)
+local frm_controls = Frame:Create(5,-13, nil,nil, '')
+local frm_groups = Frame:Create(5, -13,  nil,nil, "", nil, 12)
+local btn_add = Button:Create(nil, nil, 'control', " ADD", nil, nil,nil, nil, 40, 25)
+local btn_clear = Button:Create(nil, nil, 'control'," CLR", nil, nil, nil, nil, 40, 25)
+local search = TextField:Create(nil, nil, 150, 22, "Search...", false, false)
+local btn_add_group = Button:Create(nil, nil, 'control', '+', nil, nil, nil, nil, 20,20)
+local btn_prev_group = Button:Create(nil, nil, 'control', "<", nil,nil,nil,nil,20,20)
+local btn_next_group = Button:Create(nil, nil, 'control', ">", nil,nil,nil,nil,20,20)
+local page = Page:Create(nil,nil, 150,nil,'ui')
 local bookmarks = {}
+local groups = {}
 local clickTimer = -1
 local debug = false
 local dockstate = 0
+
+
 
 
 function load_global_settings()
@@ -40,6 +49,11 @@ function save_global_settings()
 
 end
 
+function add_group()
+	local retval, name = reaper.GetUserInputs( "Group Name", 1, 'Group Name:', 'Name' )	
+	table.insert(groups, Toggle:Create(nil, nil, 'group', name, false, 150, 25 ))
+end
+
 function new_bookmark()
 	local context = reaper.MIDIEditor_GetActive() or -1
 	local selectedItems = 1
@@ -67,6 +81,8 @@ function new_bookmark()
 		-- now let's check for and prevent duplicate bookmarks
 		for ii, b in ipairs(bookmarks) do
 			if b.item == item then goto pass end
+			-- TODO: add a setting to show multiple takes with same name
+			--if b.name == stringNeedBig then goto pass end
 		end
 
 		-- now acquire the name of the parent track, as well as it's color
@@ -75,9 +91,9 @@ function new_bookmark()
 
 
 		-- create the button for it, add it into bookmarks, don't worrk about the x/y since we
-		-- will run update_button_position() anyway. 
-		table.insert(bookmarks, Button:Create(nil, nil, trackName:sub(1,20), stringNeedBig:sub(1,20), editor, take, item, 150, 28, color))
-		update_button_position()
+		-- will run update_ui() anyway. 
+		table.insert(bookmarks, Button:Create(nil, nil, 'bookmark', trackName, stringNeedBig, take, item, track, 150, 25, color))
+		update_ui()
 		
 		-- in case it's a duplicate, go on to the next item
 		::pass::
@@ -86,7 +102,7 @@ end
 
 
 
-function update_button_position()
+function update_ui()
 --[[	
 		So now we need to run thruogh the table of book marks and move their positions relative to 
 		one aother and the dimensions of the gfx window
@@ -96,16 +112,119 @@ function update_button_position()
 		2. Place them to the right of the previouse btn..if they exceed the window's edge then reset their X
 			and increase their y (relative to the buttons above) 
 ]]--
+	local vertical 
+	if gfx.w < gfx.h then vertical = true else vertical = false end
+
+	if vertical then
+		frm_controls.w = gfx.w-10
+		frm_controls.h = 70
+
+		frm_groups.x = 5
+		frm_groups.y = frm_controls.y+frm_controls.h+5
+		frm_groups.w = gfx.w-10
+		frm_groups.h = 185
+
+		btn_add.x = frm_controls.x+5
+		btn_add.y = frm_controls.y+22
+		btn_clear.x = btn_add.x+btn_add.w+5
+		btn_clear.y = btn_add.y
+
+		search.x=frm_controls.x+7
+		search.y=btn_add.y+btn_add.h+5
+		search.w = frm_controls.w-12
+	else
+		frm_controls.w = 207
+		frm_controls.h = gfx.h-7
+
+		frm_groups.x = frm_controls.x+frm_controls.w+5
+		frm_groups.w = 315
+		frm_groups.h = gfx.h-7
+		frm_groups.y = -13
+		btn_add.x = frm_controls.x+5
+		btn_add.y = frm_controls.y+22
+		btn_clear.x = btn_add.x+btn_add.w+5
+		btn_clear.y = btn_add.y
+
+		search.x=frm_controls.x+7
+		search.y=btn_add.y+btn_add.h+5
+		search.w = frm_controls.w-12
+	end
+
+	btn_add_group.x = frm_groups.x+5
+	btn_add_group.y = frm_groups.y+20
+	btn_prev_group.x = btn_add_group.x+btn_add_group.w+5
+	btn_prev_group.y = btn_add_group.y
+
+	page.x = btn_prev_group.x+btn_prev_group.w+5
+	page.y = btn_add_group.y+6
+
+	btn_next_group.x = page.x+page.w+5
+	btn_next_group.y = btn_prev_group.y
+
+
+
+-- Place groups
+	
+	page.pages = math.ceil(#groups/6)
+	if page.pages == 0 then page.pages  = 1 end
+	page.page = 1
+
+	for i, b in ipairs(groups) do
+		if not groups[i-1] then 						
+			if not vertical then 
+				b.x = btn_add_group.x
+				b.y = btn_add_group.y+btn_add_group.h+5
+			else
+				b.x = frm_groups.x+5
+				b.y = frm_groups.y + 40
+			end
+		else 	
+			if not vertical then 
+													
+				b.x = groups[i-1].x + 155			
+				b.y = groups[i-1].y
+				if b.x+b.w >= frm_groups.x+frm_groups.w-3 then
+					b.x = frm_groups.x + 5
+					b.y = groups[i-1].y + 26
+				end
+			else
+				b.x = groups[i-1].x + 155			
+				b.y = groups[i-1].y
+				if b.x+b.w >= frm_groups.x+frm_groups.w-3 then
+					b.x = btn_add.x
+					b.y = groups[i-1].y + 26
+				end
+			end
+		end
+	end
+
+
+-- Place bookmarks
 	for i, b in ipairs(bookmarks) do
 		if not bookmarks[i-1] then 						--[1]
-			b.x = 55
-			b.y = 10
-		else 											--[2]
-			b.x = bookmarks[i-1].x + 155			
-			b.y = bookmarks[i-1].y
-			if b.x+b.w >= gfx.w-10 then
-				b.x = 55
-				b.y = bookmarks[i-1].y + 30
+			if not vertical then 
+				b.x = frm_groups.x+frm_groups.w+5
+				b.y = 3
+			else
+				b.x = 10
+				b.y = frm_groups.y+frm_groups.h + 25
+			end
+		else 	
+			if not vertical then 
+													--[2]
+				b.x = bookmarks[i-1].x + 155			
+				b.y = bookmarks[i-1].y
+				if b.x+b.w >= gfx.w-7 then
+					b.x = frm_groups.w+frm_groups.x + 5
+					b.y = bookmarks[i-1].y + 26
+				end
+			else
+				b.x = bookmarks[i-1].x + 155			
+				b.y = bookmarks[i-1].y
+				if b.x+b.w >= gfx.w-7 then
+					b.x = btn_add.x
+					b.y = bookmarks[i-1].y + 26
+				end
 			end
 		end
 	end
@@ -148,11 +267,13 @@ function next_editor()
 end
 
 function clear_all_bookmarks(closeWindow)
-	for i = #bookmarks, 1, -1 do
-		table.remove(Elements, i+2)
+
+	for e = #Elements, 1, -1 do
+		
+		if Elements[e].btype == "bookmark" then table.remove(Elements, e) end
 	end
 	bookmarks = {}
-	update_button_position()
+	update_ui()
 	if closeWindow then reaper.Main_OnCommand(40716, 0) end
 end
 
@@ -173,7 +294,7 @@ load_global_settings()
 
 gfx.init("MIDI Editor Tray", 225,500, dockstate)
 
-
+update_ui()
 
 
 function main()
@@ -199,9 +320,13 @@ function main()
 
 	end
 
+	-- Search
+	if search.leftClick then search.active = true end
+
 	-- Creates a bookmark
 	if btn_add.leftClick then 
 		new_bookmark()
+
 	end
 
 	-- Clear all bookmarks
@@ -210,6 +335,12 @@ function main()
 	elseif btn_clear.rightClick then
 		clear_all_bookmarks(true)
 
+	end
+
+	-- Group Add button
+	if btn_add_group.leftClick then
+		add_group()
+		update_ui()
 	end
 
 	-- If the user is dragging then disable buttons
@@ -235,11 +366,12 @@ function main()
 			else b:restore_ME() end 
 				b.lastClick = 0
 		end
-		if b.rightClick and clickTimer  < 0 then 
+		if b.rightClick and clickTimer  < 0 and b.btype == 'bookmark'  then 
 			table.remove(bookmarks, i)
-			--WARNING: The i+1 is to help lua find the button. If there is no button before the bookmarks begin then remove +2
-			table.remove(Elements, i+2)
-			update_button_position()
+			for ii = #Elements, 1, -1 do
+				if Elements[ii].take == b.take then table.remove(Elements, ii) ; break end
+			end
+			update_ui()
 			clickTimer = 5
 		end
 
@@ -254,21 +386,22 @@ function main()
 		return
 	elseif char == 26 and gfx.mouse_cap == 12 then reaper.Main_OnCommand(40030, 0)
 	elseif char == 26 then reaper.Main_OnCommand(40029, 0)
-
-	-- Otherwise keep window open
+	else
+		search:Change(char)
+	
 	end
 	reaper.defer(main)
 	
 -- DEBUG
 
-debug = false
-if debug then
-	cons("Bookmark count: " .. #bookmarks .. "\n")
-	for i, b in ipairs(bookmarks) do
-		cons(i .. ". " .. b.txt .. "\n" .. '\nlastclick: ' .. b.lastClick)
-	end
-	debug = false
-end
+--debug = true
+-- if debug then
+-- 	cons("Bookmark count: " .. #bookmarks .. "\n")
+-- 	for i, b in ipairs(bookmarks) do
+-- 		cons(i .. ". " .. b.txt .. "\n" .. '\nlastclick: ' .. b.lastClick)
+-- 	end
+-- 	debug = false
+-- end
 
 end
 main()
