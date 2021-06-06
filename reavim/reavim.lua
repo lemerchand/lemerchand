c65a07665b1eb549f98e20ee111fcd0f5169a486
lemerchand <phoenix.scooter@gmail.com>
2021-06-05 19:11:35 -0700

    Signed-off-by: lemerchand <phoenix.scooter@gmail.com>

---
+++ reavim/reavim.lua
@@ -0,0 +1,28 @@
+function reaperDoFile(file)
+    local info = debug.getinfo(1,'S'); script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]; dofile(script_path .. file); 
+end
+reaperDoFile('ui.lua')
+
+winwidth, winheight = 500, 250
+local mousex, mousey = reaper.GetMousePosition()
+gfx.init("reavim", winwidth, winheight, false, mousex+50,mousey-200)
+local win = reaper.JS_Window_Find("reavim", true)
+if win then reaper.JS_Window_AttachTopmostPin(win) end
+
+
+
+
+function main()
+	-- Draw the UI
+	--fill_background()
+	gfx.clear = 3092271
+	draw_elements()
+
+  char = gfx.getchar()
+  if char == 27 then reaper.atexit() else reaper.defer(main) end
+
+  if char == 'v' then gfx.print('bitch') end
+
+end
+
+main()
